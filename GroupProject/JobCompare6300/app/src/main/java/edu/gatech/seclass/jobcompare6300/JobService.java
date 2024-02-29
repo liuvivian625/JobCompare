59ad53dd878ddc583a089b63cc8496e3464a6fd6
Haochen Zhang <hc.zhang1993@gmail.com>
2024-02-29 00:56:27 -0800

    Initial backend version

---
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobService.java
@@ -0,0 +1,124 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobService {
+
+    private final JobCompareDatabase jobCompareDatabase;
+
+    public JobService(JobCompareDatabase jobCompareDatabase) {
+        this.jobCompareDatabase = jobCompareDatabase;
+    }
+
+    /**
+     * Create a new current job or update the current job if exists
+     *
+     * @param jobTitle String
+     * @param company String
+     * @param city String
+     * @param state String
+     * @param costOfLiving Float number of city indexing, must be larger than 0
+     * @param yearlySalary Float number
+     * @param yearlyBonus Float number
+     * @param numShares Float number
+     * @param homeBuyingFundPercentage Float number in [0, 15]
+     * @param personalHolidays Integer number in [0， 20]
+     * @param monthlyInternetStipend Float number in [0, 75]
+     */
+    public void addCurrentJob(String jobTitle, String company, String city, String state, Float costOfLiving,
+                                      Float yearlySalary, Float yearlyBonus, Float numShares, Float homeBuyingFundPercentage,
+                                      Integer personalHolidays, Float monthlyInternetStipend) {
+        int isCurrentJob = 1;
+
+        Job job = createJob(jobTitle, company, city, state, costOfLiving, yearlySalary, yearlyBonus,
+                numShares, homeBuyingFundPercentage, personalHolidays, monthlyInternetStipend, isCurrentJob);
+
+        if (jobCompareDatabase.fetchCurrentJob() == null) {
+            jobCompareDatabase.addJob(job);
+        } else {
+            jobCompareDatabase.updateJob(job);
+        }
+    }
+
+    /**
+     * Fetch current job information in displaying current job
+     *
+     * @return current job object
+     * @throws MissingCurrentJobException if current job has not been set
+     */
+    public Job FetchCurrentJob() throws MissingCurrentJobException {
+        Job currentJob = jobCompareDatabase.fetchCurrentJob();
+        if (currentJob == null) {
+            throw new MissingCurrentJobException();
+        }
+        return currentJob;
+    }
+
+    /**
+     * Create a new Job offer
+     *
+     * @param jobTitle String
+     * @param company String
+     * @param city String
+     * @param state String
+     * @param costOfLiving Float number of city indexing, must be larger than 0
+     * @param yearlySalary Float number
+     * @param yearlyBonus Float number
+     * @param numShares Float number
+     * @param homeBuyingFundPercentage Float number in [0, 15]
+     * @param personalHolidays Integer number in [0， 20]
+     * @param monthlyInternetStipend Float number in [0, 75]
+     */
+    public void addJobOffer(String jobTitle, String company, String city, String state, Float costOfLiving,
+                              Float yearlySalary, Float yearlyBonus, Float numShares, Float homeBuyingFundPercentage,
+                              Integer personalHolidays, Float monthlyInternetStipend) {
+        int isCurrentJob = 0;
+
+        Job job = createJob(jobTitle, company, city, state, costOfLiving, yearlySalary, yearlyBonus,
+                numShares, homeBuyingFundPercentage, personalHolidays, monthlyInternetStipend, isCurrentJob);
+
+        jobCompareDatabase.addJob(job);
+    }
+
+    /**
+     * Display current job and previously editing offer
+     *
+     * @return List of jobs which contains two job object, the first is current job, the second is job offer
+     * @throws MissingCurrentJobException if current job has not been set
+     */
+    public List<Job> displayCurrentJobAndOffer() throws MissingCurrentJobException{
+        List<Job> jobs = new ArrayList<>();
+
+        Job currentJob = jobCompareDatabase.fetchCurrentJob();
+        if (currentJob == null) {
+            throw new MissingCurrentJobException();
+        }
+        jobs.add(currentJob);
+
+        List<Job> prevJobOffers = jobCompareDatabase.fetchAllJobs();
+        Job lastOffer;
+        for(int i = prevJobOffers.size() - 1; i >= 0; i--) {
+            Job offer = prevJobOffers.get(i);
+            if(!offer.isCurrentJob()) {
+                lastOffer = offer;
+                break;
+            }
+        }
+        jobs.add(lastOffer);
+
+        return jobs;
+    }
+
+    private Job createJob(String jobTitle, String company, String city, String state, Float costOfLiving,
+                          Float yearlySalary, Float yearlyBonus, Float numShares, Float homeBuyingFundPercentage,
+                          Integer personalHolidays, Float monthlyInternetStipend, int isCurrentJob) {
+        float adjustedYearlySalary = yearlySalary * 100 / costOfLiving;
+        float adjustedYearlyBonus = yearlyBonus * 100 / costOfLiving;
+        float score = 1f;
+
+        return new Job(0, jobTitle, company, new Location(city, state), costOfLiving, yearlySalary,
+                adjustedYearlySalary, yearlyBonus, adjustedYearlyBonus, numShares, homeBuyingFundPercentage,
+                personalHolidays, monthlyInternetStipend, isCurrentJob, score);
+    }
+}
--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
@@ -2,24 +2,34 @@ package edu.gatech.seclass.jobcompare6300;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     private JobCompareDatabase jobCompareDatabase;
+    private JobService jobService;
+    private RankJobService rankJobService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Service initialization
         jobCompareDatabase = new JobCompareDatabase(this);
+        jobService = new JobService(jobCompareDatabase);
+        rankJobService = new RankJobService(jobCompareDatabase);
 
-        Job job = new Job(1,"Software Engineer", "GaTech", new Location("Atlanta", "Georgia"),10000.0f, 100000.0f, 75000.0f, 10000.0f, 7500.0f, 1000.0f, 5.0f, 15, 25.0f, 1.0f, 5.0f);
-
-        //adding sample job
-        jobCompareDatabase.addJob(job);
-
+        // TODO: GUI Interfaces
     }
 
+    public void handleClick(View view) {
+        jobService.EnterJobOffer();
+    }
 }
