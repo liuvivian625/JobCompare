7889c6afafb25d403d421cde7ccc95a27316a0ca
Jing Liu <jliu3146@gatech.edu>
2024-03-04 00:03:14 -0800

    fixed the compare button issue on MainActivity

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
@@ -2,15 +2,22 @@ package edu.gatech.seclass.jobcompare6300;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainActivity extends AppCompatActivity {
     private JobCompareDatabase jobCompareDatabase;
 
     private RankJobService rankJobService;
+    private static final Logger logger = Logger.getLogger(MainActivity.class.getName());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +27,6 @@ public class MainActivity extends AppCompatActivity {
         jobCompareDatabase = ((MyApplication) getApplication()).getJobCompareDatabase();
 
         rankJobService = new RankJobService(jobCompareDatabase);
-
-        //jobCompareDatabase.resetDb();
-
-
-//        Job job = new Job("Software Engineer", "GaTech", new Location("Atlanta", "Georgia"), 10000.0f, 100000.0f, 75000.0f, 10000.0f, 7500.0f, 1000.0f, 5.0f, 15, 25.0f, 1, 5.0f);
-//
-//        //adding sample job
-//        jobCompareDatabase.addJob(job);
-//
-//        Job job2 = new Job("Data Scientist", "Apple", new Location("Cupertino", "California"), 1000.0f, 100000.0f, 75000.0f, 10000.0f, 7500.0f, 1000.0f, 5.0f, 15, 25.0f, 1, 50.0f);
-//
-//        //adding sample job
-//        jobCompareDatabase.addJob(job2);
-//
-//        //updating sample job
-//        Job newJob = new Job("Software Engineer 2", "GaTech", new Location("Atlanta", "Georgia"), 10000.0f, 100000.0f, 75000.0f, 10000.0f, 7500.0f, 1000.0f, 5.0f, 15, 25.0f, 1, 5.0f);
-//        jobCompareDatabase.updateJob(newJob);
-//
-//        System.out.printf("current job: [%s]%n", jobCompareDatabase.fetchCurrentJob().getJobTitle());
-//
-//        jobCompareDatabase.resetDb();
 
 
         Button currentJob = findViewById(R.id.buttonCurrentJob);
@@ -71,16 +57,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button compareJobs = findViewById(R.id.buttonCompare);
-        //to-do:
-        //compare button to check if there are more than 2 jobs
-        //compareJobs.setEnabled(true);
-        compareJobs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, JobRankActivity.class);
-                startActivity(intent);
+        try{
+            List<Job> jobs = rankJobService.rankJobOffers();
+            if(jobs.size()>=2){
+                compareJobs.setEnabled(true);
+                compareJobs.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, JobRankActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
-        });
+        }catch (NoJobException e){
+            logger.log(Level.INFO, "No job exception");
+        }
+
+
 
     }
 }
